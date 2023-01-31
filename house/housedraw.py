b6d1484557aa0e643fec1173b65c4faa996372f0
Bikal <114442341+bikals@users.noreply.github.com>
2023-01-30 21:11:11 -0800

    Fixed Vijay's code related-comments in house

--- house/housedraw.py
+++ house/housedraw.py
@@ -1,42 +1,41 @@
-import turtle as turt
+#!/usr/bin/env python3
 
-def drawline(x1, y1, x2, y2):
+import turtle
+
+
+def draw_line(x1, y1, x2, y2):
     """
         Draws a line.
         :params: coordinates for the two ends of the line.
     """
-    turt.up()
-    turt.setpos(x1, y1)
-    turt.down()
-    turt.setpos(x2, y2)
+    turtle.up()
+    turtle.setpos(x1, y1)
+    turtle.down()
+    turtle.setpos(x2, y2)
 
-def drawrec(x1, x2, y1, y2, color='black', fill=True):
+
+def draw_rec(x1, y1, x2, y2, color='black', fill=True):
     """
-        Draws a rectangle.
+        Draws a rectangle
+        composed of vertical
+        and horizontal lines.
         x1: left x coord
         x2: right x coord
         y1: bottom y coord
         y2: top y coord
     """
-    turt.up()
-    turt.setpos(x1, y1)
-    turt.down()
-    turt.color(color)
+    turtle.color(color)
     if fill:
-        turt.begin_fill()
-    turt.setpos(x1, y2)
-    turt.setpos(x2, y2)
-    turt.setpos(x2, y2)
-    turt.setpos(x2, y1)
-    turt.setpos(x1, y1)
+        turtle.begin_fill()
+    draw_line(x1, y1, x1, y2)
+    draw_line(x1, y2, x2, y2)
+    draw_line(x2, y2, x2, y1)
+    draw_line(x2, y1, x1, y1)
     if fill:
-        turt.end_fill()
-    turt.color('black')
-    turt.up()
+        turtle.end_fill()
 
 
-
-def drawtri(x1, y1, x2, y2, x3, color='black', fill=True):
+def draw_tri(x1, y1, x2, y2, x3, color='black', fill=True):
     """
         Draws a triangle.
         x1: left x coord
@@ -45,22 +44,17 @@ def drawtri(x1, y1, x2, y2, x3, color='black', fill=True):
         y2: top y coord
         x3: right x coord
     """
-    turt.up()
-    turt.setpos(x1, y1)
-    turt.down()
-    turt.color(color)
+    turtle.color(color)
     if fill:
-        turt.begin_fill()
-    turt.setpos(x2, y2)
-    turt.setpos(x3, y1)
-    turt.setpos(x1, y1)
+        turtle.begin_fill()
+    draw_line(x1, y1, x2, y2)
+    draw_line(x2, y2, x3, y1)
+    draw_line(x3, y1, x1, y1)
     if fill:
-        turt.end_fill()
-    turt.color('black')
-    turt.up()
+        turtle.end_fill()
 
 
-def drawcircle(x, y, radius=50, color='black', fill=True):
+def draw_circle(x, y, radius=50, color='black', fill=True):
     """
         Draws a circle.
         x: x coord of center
@@ -68,130 +62,105 @@ def drawcircle(x, y, radius=50, color='black', fill=True):
         radius: radius
         color: fill color
     """
-    turt.setpos(x, y)
-    turt.down()
-    turt.color(color)
+    turtle.up()
+    turtle.setpos(x, y)
+    turtle.color(color)
+    turtle.down()
     if fill:
-        turt.begin_fill()
-    turt.circle(radius)
+        turtle.begin_fill()
+    turtle.circle(radius)
     if fill:
-        turt.end_fill()
-    turt.color('black')
-    turt.up()
+        turtle.end_fill()
 
 
-def drawwindow(x, y, color='black', fill=True):
-    drawrec(x, x + 50, y, y + 50, color, fill)
-    drawline(x + 25, y, x + 25, y + 50)
-    drawline(x, y + 25, x + 50, y + 25)
-
-
-def drawcircle(x, y, radius = 50, color = 'black', fill = True):
+def draw_window(x, y, color='blue', fill=True):
     """
-        Draws a circle.
-        x: x coord of center
-        y: y coord of center
-        radius: radius
-        color: fill color
+        Draws a window.
+        :params: coordinates for bottom left corner of window.
     """
-    turt.setpos(x, y)
-    turt.down()
-    turt.color(color)
-    if fill:
-        turt.begin_fill()
-    turt.circle(radius)
-    if fill:
-        turt.end_fill()
-    turt.color('black')
-    turt.up()
-
-def drawwindow(x, y, color='black', fill=True):
-    drawrec(x, x+50, y, y+50, color, fill)
-    drawline(x+25, y, x+25, y+50)
-    drawline(x, y+25, x+50, y + 25)
+    draw_rec(x, y, x + 0.5*d, y + 0.5*d, color, fill)
+    turtle.color('black')
+    draw_line(x + 0.25*d, y, x + 0.25*d, y + 0.5*d)
+    draw_line(x, y + 0.25*d, x + 0.5*d, y + 0.25*d)
 
 
-def drawgarage(x, y, color='black', fill=True):
-    drawrec(x, x + 125, y, y + 125, color, fill)
+def draw_garage(x, y, color='gray', fill=True):
+    """
+    Draws a garage.
+    :params: coordinates for bottom left corner of garage.
+    """
+    draw_rec(x, y, x + 1.25*d, y + 1.25*d, color, fill)
     r = y
-    while r + 10 < y + 125:
-        drawline(x + 5, r + 10, x + 120, r + 10)
-        r += 10
+    turtle.color('black')
+    while r + 0.1*d < y + 1.25*d:
+        draw_line(x + 0.05*d, r + 0.1*d, x + 1.2*d, r + 0.1*d)
+        r += 0.1*d
 
 
-def drawdoor(x, y, color='black', fill=True):
-    drawrec(x, x + 50, y, y + 100, color, fill)
-    turt.up()
-    turt.setpos(x + 40, y + 40)
-    turt.down()
-    turt.circle(4)
+def draw_door(x, y, color='black', fill=True):
+    """
+        Draws a door.
+        :params: coordinates for bottom left corner of door.
+    """
+    draw_rec(x, y, x + 0.5*d, y + d, color, fill)
+    draw_circle(x + 0.4*d, y + 0.4*d, int(0.04*d), color='yellow', fill=True)
 
 
-def drawtree(x, y):
+def draw_tree(x, y):
+    """
+        Draws a tree.
+        :params: coordinates for bottom left corner of tree.
+    """
+
     # Draw the tree trunk
-    turt.up()
-    drawrec(x, x + 50, y, y + 200, color='brown')
-    turt.down()
+    draw_rec(x, y, x + 0.5*d, y + 2*d, color='brown')
 
     # Draw the tree top
-    turt.up()
-    drawcircle(x - 25, y + 200, color='green')
-    drawcircle(x, y + 200, color='green')
-    drawcircle(x + 25, y + 200, color='green')
-    drawcircle(x + 50, y + 200, color='green')
-    drawcircle(x + 75, y + 200, color='green')
-    turt.down()
+    draw_circle(x - 0.25*d, y + 2*d, color='green')
+    draw_circle(x, y + 2*d, color='green')
+    draw_circle(x + 0.25*d, y + 2*d, color='green')
+    draw_circle(x + 0.5*d, y + 2*d, color='green')
+    draw_circle(x + 0.75*d, y + 2*d, color='green')
 
 
-def drawcloud(x, y):
-    # Draw the tree top
-    turt.up()
-    drawcircle(x - 50, y, color='grey')
-    drawcircle(x, y, color='grey')
-    drawcircle(x + 50, y, color='grey')
-    drawcircle(x - 25, y + 35, color='grey')
-    drawcircle(x + 25, y + 35, color='grey')
-    turt.down()
+def draw_cloud(x,y):
+    """
+        Draws a garage.
+        :params: coordinates for bottom left of cloud.
+    """
+
+    draw_circle(x - 0.5*d, y, color = 'grey')
+    draw_circle(x, y, color = 'grey')
+    draw_circle(x + 0.5*d, y, color = 'grey')
+    draw_circle(x - 0.25*d, y + 35, color = 'grey')
+    draw_circle(x + 0.25*d, y + 35, color = 'grey')
 
 
-    # Draw the tree top
-    turt.up()
-    drawcircle(x - 25, y+200, color='green')
-    drawcircle(x, y+200, color='green')
-    drawcircle(x + 25, y+200, color='green')
-    drawcircle(x + 50, y+200, color='green')
-    drawcircle(x + 75, y+200, color='green')
-    turt.down()
+def draw_house(x, y):
+    """
+           Draws a house.
+           :params: coordinates for bottom left corner of house.
+       """
 
-def drawcloud(x,y):
-    # Draw the tree top
-    turt.up()
-    drawcircle(x - 50, y, color = 'grey')
-    drawcircle(x, y, color = 'grey')
-    drawcircle(x + 50, y, color = 'grey')
-    drawcircle(x - 25, y + 35, color = 'grey')
-    drawcircle(x + 25, y + 35, color = 'grey')
-    turt.down()
+    turtle.Screen()
+    draw_rec(x, y, x + 4*d, y + 3*d, color='yellow')
+    draw_cloud(x - 1.5*d, y + 4*d)
+    draw_cloud(x + 5*d, y + 4*d)
 
-def drawhouse(x, y):
-    turt.Screen()
-    drawrec(x, x + 400, y, y + 300, color='#F0E68C')
-    drawcloud(x - 150, y + 400)
-    drawcloud(x + 500, y + 400)
+    draw_tree(x - 1.5*d, y)
+    draw_tree(x + 5*d, y)
 
-    drawtree(x - 150, y)
-    drawtree(x + 500, y)
+    draw_tri(x - 0.5*d, y + 3*d, x + 2*d, y + 4*d, x + 4.5*d, color='brown')
+    draw_window(x + 0.5*d, y + 2*d)
+    draw_window(x + 1.25*d, y + 2*d)
+    draw_window(x + 2.25*d, y + 2*d)
+    draw_window(x + 3*d, y + 2*d)
+    draw_garage(x + 0.25*d, y)
+    draw_garage(x + 2.5*d, y)
+    draw_door(x + 1.75*d, y, color='brown')
 
-    drawtri(x - 50, y + 300, x + 200, y + 400, x + 450, color='#8B0000')
-    drawwindow(x + 50, y + 200, color='#00BFFF')
-    drawwindow(x + 125, y + 200, color='#00BFFF')
-    drawwindow(x + 225, y + 200, color='#00BFFF')
-    drawwindow(x + 300, y + 200, color='#00BFFF')
-    drawgarage(x + 25, y, color='#A9A9A9')
-    drawgarage(x + 250, y, color='#A9A9A9')
-    drawdoor(x + 175, y, color='brown')
+    turtle.exitonclick()
 
-    turt.exitonclick()
-
-
-drawhouse(-200, -200)
+if __name__ == "__main__":
+    d = 100
+    draw_house(-2*d, -2*d)
